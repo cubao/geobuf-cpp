486dda789908d392663539d5eac5759753a417e4
TANG ZHIXIONG <dvora4tzx@gmail.com>
2023-01-14 00:07:16 +0800

    setup build

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+// https://github.com/microsoft/vscode-cpptools/issues/9692
+#if __INTELLISENSE__
+#undef __ARM_NEON
+#undef __ARM_NEON__
+#endif
+
+#include <pybind11/eigen.h>
+#include <pybind11/iostream.h>
+#include <pybind11/pybind11.h>
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+namespace py = pybind11;
+using namespace pybind11::literals;
+
+PYBIND11_MODULE(pybind11_geobuf, m)
+{
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
