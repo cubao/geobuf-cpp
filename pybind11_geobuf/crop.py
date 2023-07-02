9fc70e56ded14f402e29f240af8418b1385d407f
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-07-02 22:00:14 +0800

    not ready

--- pybind11_geobuf/crop.py
+++ pybind11_geobuf/crop.py
@@ -1,25 +1,60 @@
 import os
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple, Union  # noqa
 
-from loguru import logger
+import numpy as np
 from _pybind11_geobuf import *  # noqa
+from loguru import logger
 
-def crop_by_feature_id(input_path: str, output_dir: str, *, feature_id: str, size: Union[float, Tuple[float, float]] = 100.0,):
+
+def crop_by_feature_id(
+    input_path: str,
+    output_path: str,
+    *,
+    feature_id: str,
+    size: Union[float, Tuple[float, float]] = 100.0,
+):
+    logger.info(f"wrote to {output_path}")
+
+
+def crop_by_grid(
+    input_path: str,
+    output_dir: str,
+    *,
+    anchor_lla: Union[str, List[float]] = None,
+    grid_size: Union[float, Tuple[float, float]] = 1000.0,
+):
+    os.makedirs(os.path.abspath(output_dir), exist_ok=True)
+
+
+def crop_by_center(
+    input_path: str,
+    output_dir: str,
+    *,
+    anchor_lla: Union[str, List[float]] = None,
+    size: Union[float, Tuple[float, float]] = 1000.0,
+):
+    os.makedirs(os.path.abspath(output_dir), exist_ok=True)
+
+
+def crop_by_bbox(
+    input_path: str,
+    output_path: str,
+    *,
+    bbox: Union[str, List[float]],
+    z_center: float = None,
+    z_max_offset: float = None,
+):
+    logger.info(f"wrote to {output_path}")
+
+
+def crop_by_polygon(
+    input_path: str,
+    output_path: str,
+    *,
+    polygon: Union[str, np.ndarray],
+    z_max_offset: float = None,
+):
     pass
-
-
-def crop_by_grid(input_path: str, output_dir: str, *, anchor_lla: Union[str, List[float]] = None, grid_size: Union[float, Tuple[float, float]] = 1000.0,):
-    pass
-
-def crop_by_center(input_path: str, output_dir: str, *, anchor_lla: Union[str, List[float]] = None, size: Union[float, Tuple[float, float]] = 1000.0,):
-    pass
-
-def crop_by_bbox(input_path: str, output_path: str, *, bbox: Union[str,List[float]], z_center: float = None, z_max_offset: float = None,):
-    pass
-
-def crop_by_polygon(input_path: str, output_path: str, *, polygon: Union[str, np.ndarray], z_max_offset: float = None):
-    pass
-
 
 
 if __name__ == "__main__":
